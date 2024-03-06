e1a3f47ffd027a2b92dcbb0343ed62f3e033f11d
Sofia Hechaïchi <sofia.hechaichi@gmail.com>
2024-03-06 23:03:27 +0100

    [ADD] new animation loading page

--- frontend/src/PreLoader.jsx
+++ frontend/src/PreLoader.jsx
@@ -2,34 +2,73 @@ import React, { useEffect, useRef, useState } from 'react';
 import './style/PreLoader.css';
 import { motion } from 'framer-motion';
 
-const words = ["Hello,", "welcome", "to", "my", "portfolio","👋"]
+const words = ["Hello,", "welcome", "to", "my", "portfolio", "👋"];
 
-const LoadingPage = () => {
+const PreLoader = () => {
 	const loadingRef = useRef(null);
 	const [index, setIndex] = useState(0);
+	const pathRef = useRef(null);
+	const initialCurve = 100;
+	const duration = 1000;
+	let start;
 
 	useEffect(() => {
-		if (index == words.length - 1) return;
+		setPath(initialCurve);
 		setTimeout(() => {
-			setIndex(index + 1)
-		}, index == 0 ? 1000 : 150)
-	}, [index])
+			requestAnimationFrame(animate);
+		}, 3000);
+	}, []);
+
+	useEffect(() => {
+		if (index === words.length - 1) return;
+		setTimeout(() => {
+			setIndex(index + 1);
+		}, index === 0 ? 1000 : 150);
+	}, [index]);
+
+	const setPath = (curve) => {
+		const width = window.innerWidth;
+		const height = loaderHeight();
+		pathRef.current.setAttributeNS(
+			null,
+			"d",
+			`M0 0
+			L${width} 0
+			L${width} ${height}
+			Q${width / 2} ${height - curve} 0 ${height}
+			L0 0`
+		);
+	};
+
+	const loaderHeight = () => {
+		const loaderBounds = loadingRef.current.getBoundingClientRect();
+		return loaderBounds.height;
+	};
+
+	const animate = (timestamp) => {
+		if (start === undefined) {
+			start = timestamp;
+		}
+		const elapsed = timestamp - start;
+		loadingRef.current.style.top =
+			easeOutQuad(elapsed, 0, -loaderHeight(), duration) + "px";
+		if (elapsed < duration) {
+			requestAnimationFrame(animate);
+		}
+	};
+
+	const easeOutQuad = (time, start, end, duration) => {
+		return -end * (time /= duration) * (time - 2) + start;
+	};
 
 	return (
-		<motion.div
-			animate={{
-				x: 0,
-				y: '-100%',
-			}}
-			transition={{
-				duration: 1,
-				delay: 3
-			}}
-			ref={loadingRef}
-			className='introduction'>
-			<p>{words[index]}</p>
-		</motion.div>
+		<div ref={loadingRef} className="introduction">
+			<svg>
+				<path ref={pathRef}></path>
+				<text x="50%" y="40%" dominant-baseline="middle" text-anchor="middle" fill="white" fontSize="42">{words[index]}</text>
+			</svg>
+		</div>
 	);
 };
 
-export default LoadingPage;
+export default PreLoader;
